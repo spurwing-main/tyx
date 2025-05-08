6d0df2506398b67bc721bc1da84d026cf5723bbc
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2025-05-08 15:33:07 +0100

    large slider

--- tyx.js
+++ tyx.js
@@ -126,6 +126,115 @@ function main() {
 			}
 		);
 	};
+
+	tyx.functions.largeSlider = function () {
+		// Get all swiper containers
+		const swiperContainers = document.querySelectorAll(".large-slider_list-wrapper");
+
+		swiperContainers.forEach((container) => {
+			// Get the swiper-wrapper within the current container
+			const swiperWrapper = container.querySelector(".swiper-wrapper");
+
+			// Get all swiper-slide elements within the current container
+			const swiperSlides = container.querySelectorAll(".swiper-slide");
+
+			// Clone each swiper-slide element 4 times and append to the swiper-wrapper
+			for (let i = 0; i < 4; i++) {
+				swiperSlides.forEach((slide) => {
+					const clone = slide.cloneNode(true);
+					swiperWrapper.appendChild(clone);
+				});
+			}
+
+			const swiper = new Swiper(container, {
+				centeredSlides: true,
+				slideToClickedSlide: true /* click on slide to scroll to it */,
+				slidesPerView: 1,
+				autoplay: {
+					delay: 5000,
+				},
+				navigation: {
+					nextEl: ".splide__arrow",
+					prevEl: ".splide__arrow.splide__arrow--prev",
+				},
+				loop: true,
+				loopAdditionalSlides: 5 /* render more slides */,
+				freeMode: {
+					/* allow 'flick scrolling */ enabled: true,
+					sticky: true /* snap to slides */,
+					minimumVelocity: 0.05,
+					momentumVelocityRatio: 0.1,
+					momentumRatio: 0.5 /* dial it down a little */,
+				},
+				effect: "creative" /* enable scaling effect */,
+				creativeEffect: {
+					limitProgress: 2,
+					prev: {
+						// Slide scale
+						scale: 0.85,
+						translate: ["-100%", 0, 0],
+						origin: "right center",
+						opacity: 0.75,
+					},
+					next: {
+						// Slide scale
+						scale: 0.85,
+						translate: ["100%", 0, 0],
+						origin: "left center",
+						opacity: 0.75,
+					},
+				},
+				keyboard: {
+					enabled: true,
+					onlyInViewport: false,
+				},
+				on: {
+					sliderFirstMove: function () {
+						// console.log("sliderFirstMove");
+						const activeSlide = this.slides[this.activeIndex];
+						const prevSlide = this.slides[this.activeIndex - 1];
+						const nextSlide = this.slides[this.activeIndex + 1];
+						[activeSlide, prevSlide, nextSlide].forEach((slide) => {
+							const video = slide.querySelector("video");
+							if (video) {
+								video.loop = true;
+								video.play();
+							}
+						});
+					},
+					afterInit: function () {
+						// console.log("Swiper initialised");
+
+						const activeSlide = this.slides[this.activeIndex];
+						const video = activeSlide.querySelector("video");
+						if (video) {
+							video.loop = true;
+							video.play();
+						}
+					},
+					transitionEnd: function () {
+						// console.log("transitionEnd");
+						const activeSlide = this.slides[this.activeIndex];
+
+						this.slides.forEach((slideElement) => {
+							const video = slideElement.querySelector("video");
+							if (slideElement === activeSlide) {
+								if (video) {
+									video.loop = true;
+									video.play();
+								}
+							} else {
+								if (video) {
+									video.pause();
+								}
+							}
+						});
+					},
+				},
+			});
+		});
+	};
+
 	tyx.functions.magicCarousel = function () {
 		// check
 		var check = document.querySelector(".s-magic-carousel .splide");
@@ -832,6 +941,7 @@ function main() {
 	tyx.functions.faq();
 	tyx.functions.testimonials();
 	tyx.functions.magicCarousel();
+	tyx.functions.largeSlider();
 
 	// Initialize the randomText function after fonts are loaded
 	document.fonts.ready.then(function () {
